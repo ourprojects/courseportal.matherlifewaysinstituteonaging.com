71cc19c06c8d4c96281d49932c6160c44624e7be
JON WOODALL <ourprojects@hotmail.com>
2013-02-06 16:14:31 -0600

    Update application/views/user/pages/login.php

--- application/views/user/pages/login.php
+++ application/views/user/pages/login.php
@@ -1,15 +1,10 @@
 <?php $this->breadcrumbs = array(t('Login')); ?>
+
 <div class="small-masthead" style="background-image: url(<?php echo $this->getImagesUrl('header-login.png'); ?>);">
-	<h1 class="bottom"><?php echo t('Login'); ?></h1>
+  <h1 class="bottom"><?php echo t('Login'); ?></h1>
 </div>
 <div id="single-column">
-	<p>
-		<?php echo t('Not registered? Click'); ?>
-		<?php echo CHtml::link(t('here'), $this->createUrl('register')); ?>
-	</p>
-	<p>
-		<?php echo t('Forgot you password? Click'); ?>
-		<?php echo CHtml::link(t('here'), $this->createUrl('forgotPassword')); ?>
-	</p>
-	<?php echo $this->renderPartial('forms/login_form', array('model' => $model)); ?>
-</div>
+  <p> <?php echo t('Not registered? Click'); ?> <?php echo CHtml::link(t('here'), $this->createUrl('register')); ?> </p>
+  <p> <?php echo t('Once logged on to the course portal, no refund is available.'); ?> </p>
+  <p> <?php echo t('Forgot you password? Click'); ?> <?php echo CHtml::link(t('here'), $this->createUrl('forgotPassword')); ?> </p>
+  <?php echo $this->renderPartial('forms/login_form', array('model' => $model)); ?> </div>
