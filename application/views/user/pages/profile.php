3c2839d438b12b302b4c90a3519b6d7dbbc4d485
JON WOODALL <ourprojects@hotmail.com>
2013-02-11 21:59:35 -0600

    Update application/views/user/pages/profile.php
    added survey links for new surveys

--- application/views/user/pages/profile.php
+++ application/views/user/pages/profile.php
@@ -1,10 +1,33 @@
 <?php $this->breadcrumbs = array(t('Profile')); ?>
+
 <div class="small-masthead" style="background-image: url(<?php echo $this->getImagesUrl('header-register.png'); ?>);">
-	<h1 class="bottom"><?php echo t('Profile'); ?></h1>
+  <h1 class="bottom"><?php echo t('Profile'); ?></h1>
 </div>
 <div id="single-column">
-<?php 
-	echo $this->renderPartial('forms/profile_form', array('models' => $models));
- 	echo CHtml::link(t('Survey Questions'), $this->createUrl('profileSurvey')); 
+  <p>
+    <?php 
+echo $this->renderPartial('forms/profile_form', array('models' => $models));
 ?>
+  </p>
+  <hr />
+  <p>
+    <?php 
+echo CHtml::link(t('Extended Profile Survey'), $this->createUrl('profileSurvey')); 
+?>
+  </p>
+  <p>
+    <?php 
+echo CHtml::link(t('Pre-Course Survey'), $this->createUrl('precourseSurvey'));
+?>
+  </p>
+  <p>
+    <?php 
+echo CHtml::link(t('Post-Course Survey'), $this->createUrl('postcourseSurvey'));	
+?>
+  </p>
+  <p>
+    <?php 
+echo CHtml::link(t('Spencer Powell Brain Fitness Survey'), $this->createUrl('spencerpowellSurvey'));	
+?>
+  </p>
 </div>
