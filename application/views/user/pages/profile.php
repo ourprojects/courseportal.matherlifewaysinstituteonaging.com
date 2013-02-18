24d04e3279851affec304dc93092fbadf2981a64
JON WOODALL <ourprojects@hotmail.com>
2013-02-18 13:08:53 -0600

    added some survey description/purpose text

--- application/views/user/pages/profile.php
+++ application/views/user/pages/profile.php
@@ -25,6 +25,10 @@ echo $this->renderPartial('forms/profile_form', array('models' => $models));
 ?>
   </p>
   <hr />
+  
+   <h2 class="flowers"> <?php echo t('Resources'); ?> </h2>
+  <p><?php echo t('Our surveys are designed to help us obtain information that help with decision-making.'); ?></p>
+  <p><?php echo t('Our goal is to help better educate our participants by creating a client touch-point, a feedback system, and means to better understanding the shifting needs of the field.'); ?></p>
   <ul>
   <?php 
   	foreach($surveys as $survey): ?>
