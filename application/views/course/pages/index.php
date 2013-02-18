f201e2040fc2dc9d508f346f986437b87e76307d
JON WOODALL <ourprojects@hotmail.com>
2013-02-17 22:22:05 -0600

    added new course text

--- application/views/course/pages/index.php
+++ application/views/course/pages/index.php
@@ -8,7 +8,6 @@
   <p> <?php echo t('To help individual caregivers transition into their new role, be better prepared to 
 				manage their loved one\'s needs, and learn how to effectively practice self-care, Mather LifeWays Institute on Aging has developed
 	online programs that are designed to educate caregivers while fitting into any schedule.'); ?> </p>
-  
   <hr />
   <?php foreach($courses as $course): 
   
@@ -18,31 +17,26 @@
   	elseif($course->name === 'carecoachingonline')
   		echo CHtml::image($this->getImagesUrl('image-grocery.png'), t('groceries'), array('class' => 'image-right'));
   ?>
-  <h3>
-  	<a href="<?php echo $this->createUrl($course->name); ?>" title="<?php echo t($course->title); ?>">
-  		<?php echo t($course->title); ?>
-  	</a>
-  </h3>
+  <h3> <a href="<?php echo $this->createUrl($course->name); ?>" title="<?php echo t($course->title); ?>"> <?php echo t($course->title); ?> </a> </h3>
   <p><?php echo t($course->description); ?></p>
   <h4><?php echo t('Objectives'); ?></h4>
   <ul>
-  <?php 
+    <?php 
   foreach($course->objectives as $objective)
   	echo '<li>' . t($objective->text) . '</li>';
   ?>
   </ul>
   <hr />
   <?php endforeach; ?>
-<br /><br />
-
-
+  <p><?php echo t('More courses coming soon! Please check back later!'); ?></p>
+  <br />
+  <br />
   <h2 class="flowers"> <?php echo t('Online Education'); ?> </h2>
   <hr />
   <img src="<?php echo $this->getImagesUrl('image-nurse.png'); ?>"
 		class="image-right" />
   <h3> <a href="http://mather.connectedlearning.net/home/" target="_blank"><?php echo t('Gerontology Online program'); ?> </a> </h3>
   <p> <?php echo t('Gerontology Online is a web-based continuing education program designed for health care professionals who wish to enhance their knowledge and skills in the field of aging. This program will provide employees with valuable information about gerontology, helping them to stay abreast of the latest research and practices and it is also an excellent resource for new hires, providing them with a solid foundation while saving employers time and money by reducing training hours and ensuring employees have a basic skill set upon hiring.'); ?></p>
-  
   <p><?php echo t('This program is offered by Mather LifeWays Institute on Aging in collaboration with Rush University College of Nursing. Development was partially supported by funding from the Bureau of Health Professionals division of the Department of Health and Human Services.'); ?> </p>
   <p> <?php echo t('Objectives'); ?> </p>
   <ul>
@@ -52,36 +46,37 @@
     <li><?php echo t('Explore best practices that can be integrated into practice and/or teaching'); ?></li>
     <li><?php echo t('Self-paced certificate program (6 unique, online courses) completed within one-year'); ?></li>
   </ul>
-  <hr />
-  <h3> <a href="<?php echo $this->createUrl('#'); ?>"> <?php echo t('Spencer Powell Brain Fitness Program '); ?> </a><?php echo t(' - Coming Summer 2013'); ?> </h3>
-  <p> <?php echo t('The Spencer Powell Brain Fitness Program is designed to promote cognitive health and healthy lifestyle changes.  The course provides information on how lifestyle factors such as physical activity and cognitive engagement affect your brain and your risk for dementia. Practical strategies are suggested for maintaining memory over time. In addition, the course includes memory training such as chunking, the story method, and mnemonic techniques.'); ?> </p>
-  <p> <?php echo t('Objectives'); ?> </p>
-  <ul>
-    <li><?php echo t('Increase participants’ knowledge of “brain healthy” behaviors.'); ?></li>
-    <li><?php echo t('Improve self-rated memory and mindfulness/attention'); ?></li>
-    <li><?php echo t('Increase participants’ knowledge of memory techniques useful in everyday tasks and activities.'); ?></li>
-  </ul>
-<br /><br />
+  <br />
+  <br />
   <h2 class="flowers"> <?php echo t('Online Workforce Surveys'); ?> </h2>
   <hr />
-    <h3> <a href="<?php echo $this->createUrl('#'); ?>"> <?php echo t('Aging in the Workplace '); ?> </a><?php echo t(' - Coming Summer 2013'); ?> </h3>
-    <p><?php echo t('The Aging in the Workplace toolkits and survey are designed to provide employers with strategic aging-related workforce management information. With a large percent of the 78 million boomers planning to remain in the workforce past traditional retirement age, organizations in the nonprofit, private, and public sectors will need tools and resources to make the best use of their knowledge and skills in order to increase organizational effectiveness and business performance.'); ?></p>
-    
-    
-    <br /><br />
-    <h2 class="flowers"> <?php echo t('Toolkits'); ?> </h2>
-  
-    <hr />
-    <h3> <a href="<?php echo $this->createUrl('#'); ?>"> <?php echo t('Aging in the Workplace '); ?> </a><?php echo t(' - Coming Summer 2013'); ?> </h3>
-    <p><?php echo t('The Aging in the Workplace toolkits and survey are designed to provide employers with strategic aging-related workforce management information. With a large percent of the 78 million boomers planning to remain in the workforce past traditional retirement age, organizations in the nonprofit, private, and public sectors will need tools and resources to make the best use of their knowledge and skills in order to increase organizational effectiveness and business performance.'); ?></p>
-    <p><?php echo t('The toolkit provides valuable information and resources for human resource professionals, managers, and employed caregivers.'); ?></p>
-    <p><?php echo t('Toolkit Includes:'); ?></p>
-    <ul>
+  <h3> <a href="<?php echo $this->createUrl('#'); ?>"> <?php echo t('Aging in the Workplace'); ?> </a><?php echo t(' - Coming Summer 2013'); ?> </h3>
+  <p><?php echo t('The Aging in the Workplace toolkits and survey are designed to provide employers with strategic aging-related workforce management information. With a large percent of the 78 million boomers planning to remain in the workforce past traditional retirement age, organizations in the nonprofit, private, and public sectors will need tools and resources to make the best use of their knowledge and skills in order to increase organizational effectiveness and business performance.'); ?></p>
+  <br />
+  <br />
+  <h2 class="flowers"> <?php echo t('Toolkits'); ?> </h2>
+  <hr />
+  <h3> <a href="<?php echo $this->createUrl('#'); ?>"> <?php echo t('Aging in the Workplace'); ?> </a><?php echo t(' - Coming Summer 2013'); ?> </h3>
+  <p><?php echo t('The Aging in the Workplace toolkits and survey are designed to provide employers with strategic aging-related workforce management information. With a large percent of the 78 million boomers planning to remain in the workforce past traditional retirement age, organizations in the nonprofit, private, and public sectors will need tools and resources to make the best use of their knowledge and skills in order to increase organizational effectiveness and business performance.'); ?></p>
+  <p><?php echo t('The toolkit provides valuable information and resources for human resource professionals, managers, and employed caregivers.'); ?></p>
+  <p><?php echo t('Toolkit Includes:'); ?></p>
+  <ul>
     <li><?php echo t('Tools to diagnose the prevalence of caregiving in your workplace'); ?></li>
     <li><?php echo t('Team activities to support the development of a caregiver-friendly workplace'); ?></li>
     <li><?php echo t('A list of organizational best practices in elder care policies and employee benefits'); ?></li>
     <li><?php echo t('Case studies'); ?></li>
     <li><?php echo t('Information and resources for employed caregivers'); ?></li>
     <li><?php echo t('Aging in the Workplace Survey*'); ?> </li>
+  </ul>
+  <hr />
+  <h3> <a href="<?php echo $this->createUrl('#'); ?> "> <?php echo t('Making Sense of Memory Loss'); ?> </a> <?php echo t(' - Coming Summer 2013'); ?></h3>
+  <p><?php echo t('Making Sense of Memory Loss is a five-part training program for professionals interested in educating families and staff about caring for persons with memory loss in regards to its causes and treatments, as well as effective ways of coping now and in the future. In light of the growing trend toward earlier diagnosis of Alzheimer\'s Disease and related dementias, Making Sense of Memory Loss is for anyone who cares for someone in the early stages of memory loss, whether or not that the person has received a diagnosis.'); ?></p>
+  <p><?php echo t('Developed by the Alzheimer’s Association—Greater Illinois Chapter and Mather LifeWays Institute on Aging, this evidence-based training program covers these topics:'); ?></p>
+  <ul>
+    <li><?php echo t('Memory loss and related symptoms'); ?></li>
+    <li><?php echo t('Improving communication'); ?></li>
+    <li><?php echo t('Making decisions'); ?></li>
+    <li><?php echo t('Planning for the future'); ?></li>
+    <li><?php echo t('Coping and caring strategies'); ?></li>
   </ul>
 </div>
