7180d1081b5f11660abf0f84a720a49f9fa1400e
JON WOODALL <ourprojects@hotmail.com>
2013-03-01 20:14:45 -0600

    updated layout and text

--- application/views/user/pages/forgotPassword.php
+++ application/views/user/pages/forgotPassword.php
@@ -1,14 +1,9 @@
 <?php $this->breadcrumbs = array(t('Forgot your password?')); ?>
+
 <div class="small-masthead" style="background-image: url(<?php echo $this->getImagesUrl('header-login.png'); ?>);">
-	<h1 class="bottom"><?php echo t('Forgot your password?'); ?></h1>
+  <h1 class="bottom"><?php echo t('Forgot your password?'); ?></h1>
 </div>
 <div id="single-column">
-	<p>
-		<?php echo t('Please enter the email you used to register with us. Once you have submitted your request, instructions will be sent to the email provided describing how to reset your password.'); ?>
-	</p>
-	<p>
-		<?php echo t('Not registered? Click'); ?>
-		<?php echo CHtml::link(t('here'), $this->createUrl('register')); ?>
-	</p>
-	<?php echo $this->renderPartial('forms/user_maintenance_form', array('models' => $models)); ?>
-</div>
+  <p> <?php echo t('Please enter the email you used to register with us. Once you have submitted your request, instructions will be sent to the email provided describing how to reset your password.'); ?> </p>
+  <p> <?php echo CHtml::link(t('Not registered?'), $this->createUrl('register')); ?> </p>
+  <?php echo $this->renderPartial('forms/user_maintenance_form', array('models' => $models)); ?> </div>
