04c3da4c9b8eedebb97c6d1d2e758948542a1331
JON WOODALL <ourprojects@hotmail.com>
2013-03-01 20:14:16 -0600

    updated text and layout per clients

--- application/views/user/pages/register.php
+++ application/views/user/pages/register.php
@@ -4,10 +4,18 @@
   <h1 class="bottom"><?php echo t('Register'); ?></h1>
 </div>
 <div id="single-column">
-  <p> <?php echo t('Lost your account activation email? Click'); ?> <?php echo CHtml::link(t('here'), $this->createUrl('resendActivation')); ?> </p>
-  
-  <!-- disclaimer text here --> 
-  
-  <p> <?php echo t('Once logged on to the course portal, no refund is available.'); ?>
-  </p>
-  <?php echo $this->renderPartial('forms/register_form', $models); ?> </div>
+  <h2 class="flowers"><?php echo t('Register'); ?></h2>
+  <hr />
+  <p><?php echo t('Please register here to access this site. After completing the fields below, you will be sent a verification email to authenticate your email adddress. If you are a EMPLOYEE at one of the following COMPANIES, YOU MUST use your EMPLOYEE EMAIL address TO REGISTER:'); ?></p>
+  <ul>
+    <li>IBM</li>
+    <li>ExxonMobil</li>
+    <li>Merck</li>
+    <li>Texas Instruments</li>
+    <li>Deloitte</li>
+    <li>XTO Energy</li>
+    <li>Schering-Plough</li>
+  </ul>
+  <div class="box-white"> <?php echo $this->renderPartial('forms/register_form', $models); ?></div>
+  <p> <?php echo CHtml::link(t('Resend Activation Email'), $this->createUrl('resendActivation')); ?> </p>
+</div>
