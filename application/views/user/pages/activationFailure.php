986897bf57314f1cc9dd9cb405a59e39dc7cdb3e
JON WOODALL <ourprojects@hotmail.com>
2013-03-13 15:36:04 -0500

    corrected spelling error on page

--- application/views/user/pages/activationFailure.php
+++ application/views/user/pages/activationFailure.php
@@ -1,6 +1,6 @@
 <?php $this->breadcrumbs = array(t('Activation Failed')); ?>
 <p class="header">
-	<?php echo t('Oops! An error ocurred while trying to activate your acount.'); ?>
+	<?php echo t('Oops! An error occurred while trying to activate your account.'); ?>
 </p>
 <p class="header">
 	<?php echo t('Please') . ' ' . CHtml::link(t('email'), $this->createUrl('home/contact')). ' ' .
