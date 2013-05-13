e94931faae92666b9a857dcb48ce33dd9c85ee78
MacBookAir <ourprojects@hotmail.com>
2013-05-12 21:35:50 -0500

    revised layout for admin index page

--- application/views/admin/pages/index.php
+++ application/views/admin/pages/index.php
@@ -2,7 +2,6 @@
 Yii::app()->getClientScript()->registerCssFile($this->getStylesUrl('index.css')); 
 $this->breadcrumbs = array(t('Admin'));
 ?>
-<!-- NEED TO FIX THE REST, PLEASE DON'T CHANGE LAYOUT YET -->
 
 <div class="small-masthead" style="background-image: url(<?php echo $this->getImagesUrl('159325162.png'); ?>);">
   <h1 class="bottom">{t}Administrator{/t}</h1>
@@ -16,11 +15,29 @@ $this->breadcrumbs = array(t('Admin'));
     <p>{t}An application programming interface (API) is a protocol intended to be used as an interface by software components to communicate with each other. An API is a library that may include specification for routines, data structures, object classes, and variables.{/t}</p>
     <p id="categories"><a href="<?php echo $this->createUrl('apiKeys'); ?>" class="button">{t}API Keys{/t}</a> </p>
   </div>
-  <ul id="categories">
-    <li> <a href="<?php echo $this->createUrl('course'); ?>" class="button">{t}Courses{/t}</a> </li>
-    <li> <a href="<?php echo $this->createUrl('/translate/translate'); ?>" class="button">{t}Translations &amp; Languages{/t}</a> </li>
-    <li> <a href="<?php echo Yii::app()->phpBB->getACPUrl(); ?>" class="button">{t}phpBB Administration Control Panel{/t}</a> </li>
-    <li> <a href="<?php echo $this->createUrl('users'); ?>" class="button">{t}Users{/t}</a> </li>
-    <li> <a href="https://my.hostmysite.com/single.html" class="button">{t}HostMySite Control Panel{/t}</a> </li>
-  </ul>
+  <div class="box-white">
+    <h5>{t}Courses{/t}</h5>
+    <p>{t}Mather LifeWays Institute on Aging online course portal provides web-based traning and education for those who provide services to older adults. Courses target Nurses, CNAs, Social Workers, Geriatric Professionals, professional and non-professional caregivers. {/t}</p>
+    <p id="categories"><a href="<?php echo $this->createUrl('course'); ?>" class="button">{t}Courses{/t}</a> </p>
+  </div>
+  <div class="box-white">
+    <h5>{t}Translation &amp; Languages{/t}</h5>
+    <p>{t}With Google Translate, you can dynamically translate text between thousands of language pairs. The Google Translate API lets websites and programs integrate with Google Translate programmatically. Google Translate API is available as a paid service.{/t}</p>
+    <p id="categories"> <a href="<?php echo $this->createUrl('/translate/translate'); ?>" class="button">{t}Translations &amp; Languages{/t}</a> </p>
+  </div>
+  <div class="box-white">
+    <h5>{t}phpBB{/t}</h5>
+    <p>{t}phpBB is a free flat-forum bulletin board software solution that can be used to stay in touch with a group of people or can power your entire website. With an extensive database of user-created modifications and styles database containing hundreds of style and image packages to customise your board, you can create a very unique forum in minutes.{/t}</p>
+    <p id="categories"> <a href="<?php echo Yii::app()->phpBB->getACPUrl(); ?>" class="button">{t}phpBB Administration Control Panel{/t}</a> </p>
+  </div>
+  <div class="box-white">
+    <h5>{t}Course Portal User Table{/t}</h5>
+    <p>{t}User registration table records all events and user registration and profile data text input fields. Administrators have access to this table and permissions to edit, add, remove (delete) profiles.{/t}</p>
+    <p id="categories"> <a href="<?php echo $this->createUrl('users'); ?>" class="button">{t}Users{/t}</a> </p>
+  </div>
+  <div class="box-white">
+    <h5>{t}HostMySite.com cPanel{/t}</h5>
+    <p>{t}HostMySite is a division of Hosting.com, a leader in enterprise hosting solutions such as Cloud Hosting, Dedicated Hosting, Disaster Recovery, and Business Continuity Services. Hosting.com owns and operates its own datacenters and networks, employs more than 325 employees, and has been financially stable and profitable since its inception in 1997. Our passion for customer service and our carrier-class resources and facilities are what enable us to offer enterprise class service at affordable small business prices on HostMySite.{/t}</p>
+    <p id="categories"> <a href="https://my.hostmysite.com/single.html" class="button">{t}HostMySite Control Panel{/t}</a> </p>
+  </div>
 </div>
