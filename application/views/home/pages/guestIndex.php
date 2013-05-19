199f03653c92f5c1f607ef20fcc0d277a2b8ddda
MacBookAir <ourprojects@hotmail.com>
2013-05-19 14:58:12 -0500

    more revisions to the tutorial w/ new images

--- application/views/home/pages/guestIndex.php
+++ application/views/home/pages/guestIndex.php
@@ -218,7 +218,7 @@ $this->widget(
       <img src="<?php echo $this->getImagesUrl('tutorial/127032880.png'); ?>" alt="image" />
       <h4>{t}Caregiving in America{/t}</h4>
       <p>{t}Caring for a person with Alzheimer’s disease is physically, emotionally, and financially challenging. The demands of day-to-day care, changing family roles, and difficult decisions about placement in a care facility can be hard to handle. Researchers have learned much about Alzheimer’s caregiving, and studies are testing new ways to support caregivers.{/t}</p>
-      <p id="data">{t}More than <b>40 million Americans provide care</b> for relatives or friends with a chronic illness such as dementia, stroke, or Parkinson’s disease.{/t}</p>
+      <p id="data">{t}+<b>40 million Americans</b> provide care for someone with a chronic illness such as dementia.{/t}</p>
       <p>{t}Becoming well-informed about the disease is one important long-term strategy. <a href="http://www.matherlifewaysinstituteonaging.com" target="_blank">Mather LifeWays Institute on Aging</a> provides these courses to teach families about the various stages of Alzheimer’s and about flexible and practical strategies for dealing with difficult caregiving situations. We provide vital training and online education to those who care for people with Alzheimer’s and other related conditions.{/t}</p>
       <div id="question1" class="question">
         <p><b>{t}Estimated cost to employers for employees with caregiving responsibilities is in the billions of dollars.{/t}</b><br />
@@ -229,67 +229,54 @@ $this->widget(
           </select>
         </p>
         <p class="right-answer hide"> {t}Correct! According to the The MetLife Caregiving Cost Study (2013), the costs are in the billions of dollars.{/t} </p>
-        <p class="wrong-answer hide"> {t}The costs associated are in the billions of dollars. Please review the {/t} </p>
+        <p class="wrong-answer hide"> {t}The costs associated are in the billions of dollars. Please review <a href="https://www.metlife.com/assets/cao/mmi/publications/studies/2011/mmi-caregiving-costs-working-caregivers.pdf" target="_blank">The MetLife Caregiving Cost Study (2013)</a>{/t} </p>
       </div>
     </div>
     <div class="buttons"><a href="javascript:;" class="button right" onclick="$.fancybox.next();">{t}Begin Tutorial &raquo;{/t} </a></div>
   </div>
   <div id="slide-2" class="course-slide">
     <div class="content">
-      <h2 class="flowers"> {t}Find Balance{/t} </h2>
+      <h2 class="flowers"> {t}Finding Balance{/t} </h2>
       <hr />
-      <p> {t}Memory loss and other signs of mental decline have profound effects on the lives of individuals and families. But we are
-        convinced that a good quality of life can still be maintained for all concerned by learning to make changes in lifestyle and outlook. For
-        many family members, this involves a change in relationships and priorities. At times, the demands may seem overwhelming, but
-        understanding the type of cognitive impairment can be the first step in combating a challenging situation.{/t} </p>
-      <p> <b>{t}Tutorial Objectives{/t}</b> </p>
-      <ul>
-        <li>{t}Describe Alzheimer's disease amd dementia{/t}</li>
-        <li>{t}Describe the relationship between Alzheimer's disease and dementia{/t}</li>
-      </ul>
-      <p> {t}Alzheimer's disease is an irreversible, progressive brain disease that slowly destroys memory, thinking skills, behavior, and
-        eventually even the ability to carry out the simplest tasks of daily living. Symptoms usually develop slowly, worsen over time, and first appear after age 60. Alzheimer's disease is the most common form and cause of dementia among older people. Alzheimer's has no current cure. The disease is named after Dr. Alois Alzheimer.{/t} </p>
-      <ul>
-        <li>{t}Millions of Americans are living with Alzheimer's disease.{/t} </li>
-        <li>{t}Alzheimer's is not a normal part of aging{/t} </li>
-      </ul>
-      <p>
+      <p> {t}<b>Caregiving is not just a social issue, it is a critical workplace issue</b>. Caregiving for a relative with a chronic condition or for an elderly person is usually provided by a family member, and typcially these family members are employed.{/t}</p>
+      <p>{t}According to the <a href="http://www.alz.org/downloads/facts_figures_2012.pdf" target="_blank">Alzheimer's disease Facts and Figures (2012)</a> report, a large percentage of the American workforce is estimated to be involved in caregiving. And, as the number of elderly Americans grows, the number of employed caregivers—and the billions in annual cost to business, in addition to the billions associated with health care costs — is likely to continue upwards.{/t}</p>
+      
+      <p>{t}In fact, as reported, in the <b>next two decades</b>, the <b>number of older Americans will more than double</b>. The fastest growing segment of the population are those age 85 and older, a segment that is most likely to be frail and have chronic conditions. Given rising healthcare costs and shorter hospital stays, the care of these older Americans will increasingly fall on their children, friends, and other relatives — unpaid caregivers who are, for the most part, still in the workforce. Many of them are at the peak of their careers.{/t}</p>
+      
+      <p>{t}The current figures are already staggering. Today, <b>millions of caregivers provide assistance to people age 50 and older</b>, and nearly all of the care of older adults is provided by family and friends. Meanwhile, more than <b>half of the nation’s unpaid caregivers are in the workforce</b>.{/t}</p>      
+      
+        <p>
         <iframe width="400" height="225" src="http://www.youtube.com/embed/In1IJocVor8?rel=0" frameborder="0" allowfullscreen></iframe>
       </p>
-      <p> <b> {t}What to look for if you suspect someone is suffering from Alzheimer's disease:{/t} </b> </p>
-      <ul>
-        <li>{t}Challenges in planning or solving problems{/t}</li>
-        <li>{t}Difficulty completing familiar tasks at home, at work or at leisure{/t} </li>
-      </ul>
-      <p class="link">{t}Alzheimer's disease Facts and Figures 2012 (USA / English) - {/t}</p>
-      <p><a href="http://www.alz.org/downloads/facts_figures_2012.pdf" target="_blank"><img class="normal" width="64" height="64" src="<?php echo $this->GetImagesURL('pdf-icon.png'); ?>" alt="{t}.pdf icon{/t}" /></a> </p>
-      <p>&nbsp; </p>
-      <p>&nbsp;</p>
+     
+
     </div>
     <div class="buttons"><a href="javascript:;" class="button left" onclick="$.fancybox.prev();">&laquo;&nbsp;{t}Back{/t} </a> <a href="javascript:;" class="button right" onclick="$.fancybox.next();">{t}Next{/t}&nbsp;&raquo; </a></div>
   </div>
   <div id="slide-3" class="course-slide">
     <div class="content">
-      <h2 class="flowers"> {t}Demographics{/t} </h2>
+      <h2 class="flowers">{t}Demographics{/t}</h2>
       <hr />
-      <img src="<?php echo $this->getImagesUrl('78634274r.jpeg'); ?>" alt="{t}Tutorial{/t}" />
-      <p> {t}Dementia refers to an acquired and progressive loss of mental functions due to a brain disorder. Dementia is not a specific
-        disease. It is an overall term that describes a wide range of symptoms associated with a decline in memory or other thinking skills 
-        severe enough to reduce a person's ability to perform everyday activities. A medical diagnosis is required to determine the underlying 			cause or causes of symptoms.{/t} </p>
-      <p> <b>{t}Symptoms and signs of dementia{/t}</b> </p>
-      <p> {t}While symptoms of dementia can vary greatly, at least two of the following core mental functions, amongst others, must be significantly impaired to be onsidered dementia:{/t} </p>
-      <ul>
-        <li>{t}Memory{/t}</li>
-        <li>{t}Communication and language{/t}</li>
-        <li>{t}Ability to focus and pay attention{/t}</li>
+      <img src="<?php echo $this->getImagesUrl('tutorial/133300753.png'); ?>" alt="{t}image{/t}" />
+     
+     <p>{t}Various surveys show caregivers include all ages, races, and both genders. In 2009, the National Alliance for Caregiving and AARP, along with Census figures and other reports, developed a study entitled Caregiving in the U.S., found the following:{/t}</p>
+     
+     <ul>
+     	<li>{t}About 87 percent of caregivers provide care to someone over age 50, and 60 percent of those caregivers work full time.{/t}</li>
+        <li>About 15 percent of caregivers provide care to someone who lives more than an hour away.{/t}</li>
+        <li>More than a third have children or grandchildren under the age of 18 inthe household.{/t}</li>
       </ul>
-      <p> {t}People with dementia may have problems with short-term memory, keeping track of a purse or wallet, paying bills, planning and
-        preparing meals, remembering appointments or traveling out of the neighborhood. Many dementias are progressive, meaning symptoms start
-        out slowly and gradually get worse.{/t} </p>
-      <p> {t}Using your spreadsheet processor, please search the Internet for additional symptoms and signs of dementia that have not been mentioned here and list them. Once completed, please email your Facilitator the attachment.{/t}</p>
-      <p><img class="normal" width="48" height="48" src="<?php echo $this->getImagesUrl('spreadsheet.png'); ?>" /><a href="mailto:jwoodall@matherlifeways.com" /> <img class="normal" width="48" height="48" src="<?php echo $this->getImagesUrl('email.png'); ?>" /></a></p>
-      <br />
-      <br />
+      
+       <div id="question" class="question">
+        <p> {t}Search the Internet to find out how many caregivers are in your state or region.{/t} </p>
+        <form method="get" action="http://www.google.com/search" target="_blank">
+          <input type="text" id="google-search" name="q" size="65" maxlength="255" value="" />
+          <input type="submit" value="{t}Google Search{/t}" class="teal" />
+        </form>
+      </div>
+     
+     
+     
     </div>
     <div class="buttons"><a href="javascript:;" class="button left"
 				onclick="$.fancybox.prev();">&laquo;&nbsp;{t}Back{/t} </a> <a
@@ -300,26 +287,16 @@ $this->widget(
     <div class="content">
       <h2 class="flowers"> {t}The Sandwich Generation{/t} </h2>
       <hr />
-      <h4>
-      {t}Media Storm{/t}
-      <p>{t}Filmmaker-photographer couple Julie Winokur and Ed Kashi were busy pursuing their careers and raising two children when Winokur's 83-year-old father, Herbie, became too infirm to care for himself.{t}
-        
-        {t}At that moment they joined some twenty million other Americans who make up the sandwich generation, those who find themselves responsible for the care of both their children and their aging parents.{/t}
+    
+      <h4>{t}Media Storm{/t}</h4>
+      <p>{t}Filmmaker-photographer couple Julie Winokur and Ed Kashi were busy pursuing their careers and raising two children when Winokur's 83-year-old father, Herbie, became too infirm to care for himself.{t}</p>
+      
+      <p>{t}At that moment they joined some twenty million other Americans who make up the sandwich generation, those who find themselves responsible for the care of both their children and their aging parents.{/t}</p>
       <div style="width:400px;">
         <div style="height:340px;"><script type="text/javascript" src="http://mediastorm.com/player/embed.php?id=e51981bc412802480288&w=400&h=340&amp;lang=none"></script></div>
         <div style="padding:10px; font-family:Helvetica, Arial, sans-serif; font-size:12px; line-height:16px; color:#999999; background-color:#000000;">Millions of middle-aged Americans are caring for their children as well as their aging parents. When filmmaker-photographer pair Julie Winokur and Ed Kashi took in Winokur's 83-year-old father, they decided to document their own story. See the project at <a href="http://mediastorm.com/publication/the-sandwich-generation" target="_blank" style="color:#0083c5;">http://mediastorm.com/publication/the-sandwich-generation</a></div>
       </div>
-      <div id="question1" class="question">
-        <p><b>{t}Is dementia a disease of the brain?{/t}</b>
-          <select>
-            <option selected="selected" value="select"> {t}Select{/t} </option>
-            <option value="1"> {t}Yes{/t} </option>
-            <option value="0"> {t}No{/t} </option>
-          </select>
-        </p>
-        <p class="right-answer hide"> {t}Great! Yes, Dementia is a disease of the brain.{/t} </p>
-        <p class="wrong-answer hide"> {t}Please ensure you understand what dementia is. Dementia is a disease of the brain.{/t} </p>
-      </div>
+    
     </div>
     <div class="buttons"><a href="javascript:;" class="button left" onclick="$.fancybox.prev();">&laquo;&nbsp;{t}Back{/t} </a> <a href="javascript:;" class="button right" onclick="$.fancybox.next();">{t}Next{/t}&nbsp;&raquo; </a></div>
   </div>
