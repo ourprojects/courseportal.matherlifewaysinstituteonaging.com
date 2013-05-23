76fadabc53726320d0a473c93dac2e8c2f0d6217
louis89 <l.daprato@gmail.com>
2013-05-23 12:59:18 -0500

    Simplified switching in and out of debug mode in phpBB config.

--- forum/config.php
+++ forum/config.php
@@ -1,16 +1,29 @@
 <?php
-// phpBB 3.0.x auto-generated configuration file
-// Do not change anything in this file!
-$dbms = 'mysqli';
-$dbhost = 'localhost';
-$dbport = '';
-$dbname = 'courseportal_development';
-$dbuser = 'c145190-h2227182';
-$dbpasswd = 'Mj93f6d42b!';
-$table_prefix = 'phpbb_';
-$acm_type = 'file';
-$load_extensions = '';
-
 @define('PHPBB_INSTALLED', true);
 @define('DEBUG', true);
 @define('DEBUG_EXTRA', true);
+
+if(defined('DEBUG') && DEBUG)
+{
+	$dbms = 'mysqli';
+	$dbhost = 'localhost';
+	$dbport = '';
+	$dbname = 'courseportal_development';
+	$dbuser = 'c145190-h2227182';
+	$dbpasswd = 'Mj93f6d42b!';
+	$table_prefix = 'phpbb_';
+	$acm_type = 'file';
+	$load_extensions = '';
+}
+else
+{
+	$dbms = 'mysqli';
+	$dbhost = 'localhost';
+	$dbport = '';
+	$dbname = 'courseportal';
+	$dbuser = 'c145190-h222718';
+	$dbpasswd = 'Mj93f6d42b!';
+	$table_prefix = 'phpbb_';
+	$acm_type = 'file';
+	$load_extensions = '';
+}
