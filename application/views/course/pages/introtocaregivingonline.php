204ebee103e0c67ee367486fd0ecb1a36d38b739
MacBookAir <ourprojects@hotmail.com>
2013-07-24 21:19:38 -0500

    small text/layout updates to survey text and deleted metadata area on profile page

--- application/views/course/pages/introtocaregivingonline.php
+++ application/views/course/pages/introtocaregivingonline.php
@@ -29,10 +29,10 @@ foreach(array(
 </div>
 <div id="sidebar">
   <div class="box-sidebar one" style="background-color:#FFF;">
-    <h3>{t}Coming Soon!{/t}</h3>
+    <h3>{t}Evaluations - Coming Soon!{/t}</h3>
     <br />
-    <p><a href="#">{t}Pre-Course Survey{/t}</a></p>
-    <p><a href="#">{t}Post-Course Survey{/t}</a></p>
+    <p><a href="#">{t}Pre-Course Evaluation{/t}</a></p>
+    <p><a href="#">{t}Post-Course Evaluation{/t}</a></p>
     <br />
     <img src="<?php echo $this->getImagesUrl('intro/153075496.png'); ?>" alt="image"> </div>
   <div class="box-sidebar one">
--- application/views/user/pages/profile.php
+++ application/views/user/pages/profile.php
@@ -26,7 +26,7 @@ $this->widget(
   <!-- sidebar #1 here -->
   
   <div class="box-sidebar one">
-    <h3>{t}Surveys{/t}</h3>
+    <h3>{t}Optional Surveys{/t}</h3>
     <br />
     <p> {t}Our surveys are designed to help us obtain information that help with decision-making. Our goal is to help better educate our participants by creating a client touch-point, a feedback system, and means to better understanding the shifting needs of the field.{/t}</p>
     <hr />
@@ -40,48 +40,26 @@ $this->widget(
     </ul>
     <br />
   </div>
-  
-  <!-- sidebar #2 here -->
-  <div class="box-sidebar one">
-    <h3>{t}Metadata{/t}</h3>
-    <p>{t}Metadata - Metadata is data about data. Metadata describes how and when and by whom a particular set of data was collected, and how the data is formatted. Metadata is essential for understanding information stored in our database and has become increasingly important in our XML-based Web applications.{/t}</p>
-    <p>{t}Metadata (metacontent describing the details we collect) are defined as the data providing information about one or more aspects of the data, such as:{/t}</p>
-    <ul>
-      <li>{t}* Means of creation of the data{/t}</li>
-      <li>{t}* Purpose of the data{/t}</li>
-      <li>{t}* Time and date of creation{/t}</li>
-      <li>{t}* Creator or author of the data{/t}</li>
-      <li>{t}* Location on a computer network where the data were created{/t}</li>
-      <li>{t}* Standards used{/t}</li>
-    </ul>
-    <p>{t}The information you provide us, via your profile, is used for research purposes only. We do not sell, distribute, or use your information for any other purpose than helping make these online courses better.{/t}</p>
-  </div>
-  <!-- sidebar #3 here --> 
-  
 </div>
 <div class="column-wide">
   <h2 class="flowers">{t}Profile{/t}</h2>
   <p>{t}Please complete your profile. This information helps us track your participation and helps us compile important geographical data, so we have better information when updating course content.{/t}</p>
   <br />
+  <div class="box-white"> <?php echo $this->renderPartial('forms/profile_form', $models); ?> </div>
   <div class="box-white">
-	<?php echo $this->renderPartial('forms/profile_form', $models); ?>
-  </div>
-  <div class="box-white">
-  	<p>{t}Your agreements{/t}</p>
-  	<br />
-  	<?php 
+    <p>{t}Your agreements{/t}</p>
+    <br />
+    <?php 
   	$agreements = Yii::app()->getUser()->getModel()->agreements;
   	if(empty($agreements)): 
   		echo t('None');
   	else:?>
-  	<ul>
-  		<?php foreach($agreements as $agreement): ?>
-  		<li>
-  			<a href="<?php echo $this->createUrl('/agreement/' . $agreement->id) ?>" target='_blank'><?php echo t($agreement->name); ?></a>
-  		</li>
-  		<?php endforeach; ?>
-  	</ul>
-  	<?php
+    <ul>
+      <?php foreach($agreements as $agreement): ?>
+      <li> <a href="<?php echo $this->createUrl('/agreement/' . $agreement->id) ?>" target='_blank'><?php echo t($agreement->name); ?></a> </li>
+      <?php endforeach; ?>
+    </ul>
+    <?php
 	endif; 
 	?>
   </div>
