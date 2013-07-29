6f341c117192b67880cca42563404fe2b521224f
louis89 <l.daprato@gmail.com>
2013-07-29 12:49:18 -0500

    Separated forgot password email code from forgot password action code.

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,7 +1,7 @@
-<?php   
+<?php
 
 class UserController extends ApiController {
-	
+
 	const REMEMBER_ME_DURATION = 2592000; // 30 days in seconds
 
 	/**
@@ -10,7 +10,7 @@ class UserController extends ApiController {
 	public function filters() {
 		return array_merge(parent::filters(), array('accessControl + profile, profileSurvey'));
 	}
-	
+
 	public function accessRules() {
 		return array(
 				array('allow',
@@ -25,23 +25,23 @@ class UserController extends ApiController {
 	/**
 	 * Displays the login page
 	 */
-	public function actionLogin() 
-	{	
+	public function actionLogin()
+	{
 		$loginModel = new Login;
 
-		if(isset($_POST['ajax']) && $_POST['ajax'] === 'login-form') 
-		{	
+		if(isset($_POST['ajax']) && $_POST['ajax'] === 'login-form')
+		{
 			echo CActiveForm::validate($loginModel);
-			Yii::app()->end();	
+			Yii::app()->end();
 		}
 
-		if($loginModel->loadAttributes() && $loginModel->validate()) 
+		if($loginModel->loadAttributes() && $loginModel->validate())
 		{
 			$webUser = Yii::app()->getUser();
 			if($webUser->login(
-					new BasicIdentity($loginModel->username_email, $loginModel->password), 
-					$loginModel->remember_me ? self::REMEMBER_ME_DURATION : 0)) 
-			{		
+					new BasicIdentity($loginModel->username_email, $loginModel->password),
+					$loginModel->remember_me ? self::REMEMBER_ME_DURATION : 0))
+			{
 				$webUser->setFlash('success', t('Welcome {name}!', array('{name}' => $webUser->getModel()->name)));
 				$this->redirect($webUser->returnUrl);
 			}
@@ -53,48 +53,40 @@ class UserController extends ApiController {
 		$this->render('pages/login', array('model' => $loginModel));
 	}
 
-	public function actionForgotPassword() 
-	{	
+	public function actionForgotPassword()
+	{
 		$models = array(
 				'UserNameEmail' => new UserNameEmail,
 				'Captcha' => new Captcha
 		);
 		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax'] === 'user-maintenance-form') 
-		{	
+		if(isset($_POST['ajax']) && $_POST['ajax'] === 'user-maintenance-form')
+		{
 			echo CActiveForm::validate($models);
-			Yii::app()->end();	
+			Yii::app()->end();
 		}
-		
-		if($models['Captcha']->loadAttributes() && 
-				$models['UserNameEmail']->loadAttributes() && 
-				$models['Captcha']->validate() && 
-				$models['UserNameEmail']->validate()) 
+
+		if($models['Captcha']->loadAttributes() &&
+				$models['UserNameEmail']->loadAttributes() &&
+				$models['Captcha']->validate() &&
+				$models['UserNameEmail']->validate())
 		{
 			$user = $models['UserNameEmail']->getUser();
-			if($user === null) 
+			if($user === null)
 			{
 				$models['UserNameEmail']->addError('name_email', t('The username or email address you have entered could not be found.'));
-			} 
-			else 
+			}
+			else
 			{
-				if(!$user->getIsActivated()) 
-				{	
-					Yii::app()->getUser()->setFlash('error', 
+				if(!$user->getIsActivated())
+				{
+					Yii::app()->getUser()->setFlash('error',
 						t('We could not reset your password because your account has not yet been activated. To have an activation email sent to you again please click ').
 							CHtml::link(t('here'), $this->createUrl('resendActivation')));
-				} 
-				else 
-				{	
-					$this->loadExtension('yii-mail');
-					$message = Yii::app()->mail->getNewMessageInstance();
-					$message->view = 'passwordReset';
-					$message->setSubject(t('MatherLifeways Password Reset Request'));
-					$message->setBody(array('url' => $user->encodeUrl('user/changePassword')), 'text/html');
-					$message->setTo($user->email);
-					$message->setFrom(Yii::app()->params['noReplyEmail']);
-					Yii::app()->mail->send($message);
-					
+				}
+				else
+				{
+					$this->sendPasswordResetEmail($user);
 					Yii::app()->getUser()->setFlash('success', t('Instructions for resetting your password have been sent to your email.'));
 				}
 				$this->refresh();
@@ -102,7 +94,7 @@ class UserController extends ApiController {
 		}
 		$this->render('pages/forgotPassword', array('models' => $models));
 	}
-	
+
 	public function actionChangePassword($id = null, $session_key = null)
 	{
 		$ChangePassword = new ChangePassword('change');
@@ -113,7 +105,7 @@ class UserController extends ApiController {
 			$ChangePassword->setScenario('reset');
 			$ChangePassword->username_email = $UserIdentity->getModel()->name;
 		}
-		
+
 		if($ChangePassword->loadAttributes()) {
 			$ChangePassword->validate();
 			// if this is an ajax validation request
@@ -122,7 +114,7 @@ class UserController extends ApiController {
 				echo $ChangePassword->getErrorsAsJSON();
 				Yii::app()->end();
 			}
-			
+
 			if(!$ChangePassword->hasErrors() && $ChangePassword->getScenario() === 'change')
 			{
 				$UserIdentity = new BasicIdentity($ChangePassword->username_email, $ChangePassword->current_password);
@@ -131,7 +123,7 @@ class UserController extends ApiController {
 					$ChangePassword->addError('username_email_current_password', t('Incorrect username or password.'));
 				}
 			}
-			
+
 			if(!$ChangePassword->hasErrors())
 			{
 				$user = $UserIdentity->getModel();
@@ -142,7 +134,7 @@ class UserController extends ApiController {
 					$webUser = Yii::app()->getUser();
 					if($webUser->login(new BasicIdentity($user->email, $ChangePassword->new_password)))
 					{
-						$webUser->setFlash('success', 
+						$webUser->setFlash('success',
 								t('Your account password has been reset. Welcome {email}!', array('{email}' => $webUser->name)));
 						$this->redirect(Yii::app()->homeUrl);
 					}
@@ -155,40 +147,40 @@ class UserController extends ApiController {
 	/**
 	 * Displays the register page
 	 */
-	public function actionRegister() 
-	{	
+	public function actionRegister()
+	{
 		$models = array(
 					'Register' => new Register,
 					'Captcha' => new Captcha,
 				);
 		$models['Register']->agreement_id = 1;
-		
+
 		// collect user input data
-		if($models['Captcha']->loadAttributes() && $models['Register']->loadAttributes()) 
+		if($models['Captcha']->loadAttributes() && $models['Register']->loadAttributes())
 		{
 			if(isset($_POST['ajax']) && $_POST['ajax'] === 'register-form')
 			{
 				echo CActiveForm::validateTabular($models);
 				Yii::app()->end();
 			}
-			
+
 			if($models['Captcha']->validate() && $models['Register']->validate())
 			{
 				$userAgreement = new UserAgreement();
 				$userAgreement->agreement_id = $models['Register']->agreement_id;
 				$user = new CPUser();
 				$user->setAttributes($models['Register']->getAttributes());
-				
+
 				$transaction = Yii::app()->db->beginTransaction();
-				try 
+				try
 				{
-					if($user->save()) 
-					{	
+					if($user->save())
+					{
 						$userAgreement->user_id = $user->id;
 						$userAgreement->save();
 					}
-				} 
-				catch(Exception $e) 
+				}
+				catch(Exception $e)
 				{
 					$transaction->rollback();
 					throw $e;
@@ -207,28 +199,28 @@ class UserController extends ApiController {
 		$this->render('pages/register', array('models' => $models));
 	}
 
-	public function actionActivate($id, $session_key) 
+	public function actionActivate($id, $session_key)
 	{
 		$userIdentity = new SessionIdentity($id, CBase64::urlDecode($session_key), false);
 
-		if($userIdentity->authenticate()) 
+		if($userIdentity->authenticate())
 		{
 			$user = $userIdentity->getModel();
 			$user->setIsActivated(true);
-		
-			if($user->getRelated('activated')->save()) 
+
+			if($user->getRelated('activated')->save())
 			{
 				Yii::app()->getUser()->login($userIdentity, 0);
 				$user->regenerateSessionKey();
-				Yii::app()->getUser()->setFlash('success', 
-					t('Your account has been activated. Welcome {email}!', 
+				Yii::app()->getUser()->setFlash('success',
+					t('Your account has been activated. Welcome {email}!',
 						array('{email}' => Yii::app()->getUser()->name)));
 				$this->redirect(Yii::app()->homeUrl);
 			}
 		}
 		$this->render('pages/activationFailure');
 	}
-	
+
 	public function actionResendActivation() {
 		$models = array(
 				'UserNameEmail' => new UserNameEmail,
@@ -240,19 +232,19 @@ class UserController extends ApiController {
 			echo CActiveForm::validate($models);
 			Yii::app()->end();
 		}
-	
-		if($models['Captcha']->loadAttributes() && 
-				$models['UserNameEmail']->loadAttributes() && 
-				$models['Captcha']->validate() && 
-				$models['UserNameEmail']->validate()) 
+
+		if($models['Captcha']->loadAttributes() &&
+				$models['UserNameEmail']->loadAttributes() &&
+				$models['Captcha']->validate() &&
+				$models['UserNameEmail']->validate())
 		{
 			$user = $models['UserNameEmail']->getUser();
-			if($user->getIsActivated()) 
+			if($user->getIsActivated())
 			{
-				Yii::app()->getUser()->setFlash('error', t('Your account has already been activated. If you have forgotten your password you can recover it by clicking') . 
+				Yii::app()->getUser()->setFlash('error', t('Your account has already been activated. If you have forgotten your password you can recover it by clicking') .
 															'&nbsp;' . CHtml::link(t('here'), $this->createUrl('forgotPassword')));
-			} 
-			else 
+			}
+			else
 			{
 				$this->sendAccountActivationEmail($user);
 				Yii::app()->getUser()->setFlash('success', t('We have resent an activation email to') . '&nbsp;' . $user->email);
@@ -261,7 +253,19 @@ class UserController extends ApiController {
 		}
 		$this->render('pages/resendActivation', array('models' => $models));
 	}
-	
+
+	public function sendPasswordResetEmail($userModel)
+	{
+		$message = Yii::app()->mail->getNewMessageInstance();
+		$message->layout = 'mail';
+		$message->view = 'passwordReset';
+		$message->setSubject(t('MatherLifeways Password Reset Request'));
+		$message->setBody(array('url' => $userModel->encodeUrl('user/changePassword')), 'text/html');
+		$message->setTo($userModel->email);
+		$message->setFrom(Yii::app()->params['noReplyEmail']);
+		Yii::app()->mail->send($message);
+	}
+
 	public function sendAccountActivationEmail($userModel) {
 		$message = Yii::app()->mail->getNewMessageInstance();
 		$message->layout = 'mail';
@@ -282,26 +286,26 @@ class UserController extends ApiController {
 		Yii::app()->getUser()->setFlash('success', t('You have been successfully logged out.'));
 		$this->redirect(Yii::app()->homeUrl);
 	}
-	
+
 	public function actionProfile() {
 		$user = Yii::app()->getUser()->getModel();
 		$models = array(
 				'Profile' => new UserProfile,
 				'avatar' => new Avatar);
-		
+
 		$models['Profile']->setAttributes($user->getAttributes());
 		$models['avatar']->user_id = $user->id;
-		
+
 		$surveys = array();
 		foreach(array(
-				'profile', 
-				'precourse', 
-				'postcourse', 
+				'profile',
+				'precourse',
+				'postcourse',
 				'spencerpowell') as $surveyName) {
 			$survey = $this->createWidget(
 					'modules.surveyor.widgets.Survey',
 					array(
-							'id' => $surveyName, 
+							'id' => $surveyName,
 							'options' => array(
 								'htmlOptions' => array('style' => 'display:none;'),
 								'title' => array('htmlOptions' => array('class' => 'flowers')),
@@ -328,7 +332,7 @@ class UserController extends ApiController {
 		if($models['Profile']->loadAttributes() && $models['Profile']->validate() && $models['avatar']->validate())
 		{
 			$user->setAttributes($models['Profile']->getAttributes());
-			
+
 			if($user->validate())
 			{
 				$transaction = Yii::app()->db->beginTransaction();
@@ -354,51 +358,51 @@ class UserController extends ApiController {
 		}
 		$this->render('pages/profile', array('models' => $models, 'surveys' => $surveys));
 	}
-	
+
 	/****** START API ACTIONS ******/
-	
-	public function actionCreate() 
+
+	public function actionCreate()
 	{
 		$model = new Register('pushed');
-	
-		if($model->loadAttributes() && $model->validate()) 
+
+		if($model->loadAttributes() && $model->validate())
 		{
 			$user = new CPUser();
 			$user->setAttributes($model->getAttributes());
-			
-			if($user->save()) 
-			{	
+
+			if($user->save())
+			{
 				$this->sendAccountActivationEmail($user);
 				$user->attachBehavior('toArray', array('class' => 'behaviors.EArrayBehavior'));
 				return $this->renderApiResponse(200, $user->toArray(array('id', 'email'), true));
 			}
 			$model->addErrors($user->getErrors());
 		}
-		
+
 		$this->renderApiResponse(400, $model->getErrorsAsJSON());
 	}
-	
+
 	public function actionRead() {
 		$model = new CPUser('search');
-		
+
 		if(isset($_GET['CPUser']))
 			$model->setAttributes($_GET['CPUser']);
-		
+
 		$criteria = $model->getSearchCriteria();
 		$criteria->mergeWith($model->getSearchCriteria());
 		$users = $model->with('group')->findAll($criteria);
 		$data = array();
 		foreach($users as $user) {
 			$user->attachBehavior('toArray', array('class' => 'behaviors.EArrayBehavior'));
-			$data[] = $user->toArray(array_merge($user->getSafeAttributeNames(), array('group' => 'name')), true); 
+			$data[] = $user->toArray(array_merge($user->getSafeAttributeNames(), array('group' => 'name')), true);
 		}
-	
+
 		if(empty($data))
 			$this->renderApiResponse(404, $data);
 		else
 			$this->renderApiResponse(200, $data);
 	}
-	
+
 	public function actionUpdate() {
 		$requestVars = Yii::app()->getRequest()->getRestParams();
 		if(!isset($requestVars['id'])) {
@@ -407,26 +411,26 @@ class UserController extends ApiController {
 		}
 
 		$model = CPUser::model()->findByPk($requestVars['id']);
-		
+
 		$model->setAttributes($requestVars);
-		
+
 		if($model->save()) {
 			$this->renderApiResponse();
 		}
 
 		$this->renderApiResponse(400, $model->getErrorsAsJSON());
 	}
-	
+
 	public function actionDelete() {
 		$requestVars = Yii::app()->getRequest()->getRestParams();
-		if(!isset($requestVars['id'])) 
+		if(!isset($requestVars['id']))
 		{
 			return $this->renderApiResponse(400, array('id' => t('The id of the user to be deleted must be specified.')));
 		}
 
 		$this->renderApiResponse(200, array('rows_deleted' => CPUser::model()->deleteByPk($requestVars['id'])));
 	}
-	
+
 	public function actionOptions() {
 		$model = new CPUser('search');
 		$response['GET'] =
@@ -454,5 +458,5 @@ class UserController extends ApiController {
 					);
 		$this->renderApiResponse(200, $response);
 	}
-	
+
 }
