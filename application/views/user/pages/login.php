057be397c86809ab83ac5307d7ed132fddc5da64
JonWoodallWindows8Pro <ourprojects@hotmail.com>
2013-08-14 22:42:48 -0500

    corrected MLIA sidebar image bottom rounded corners

--- application/views/user/pages/login.php
+++ application/views/user/pages/login.php
@@ -1,34 +1,6 @@
 <?php $this->breadcrumbs = array('{t}Login{/t}'); ?>
 
 <div class="small-masthead" style="background-image: url(<?php echo $this->getImagesUrl('header-login.png'); ?>);">
-<<<<<<< .merge_file_99Iexi
-	<h1 class="bottom">{t}Login{/t}</h1>
-</div>
-<div id="sidebar">
-	<div class="box-sidebar one">
-		<h3>Mather LifeWays Institute on Aging</h3>
-		<p class="text-center bold">
-			<a href="http://www.matherlifewaysinstituteonaging.com/products/" target="_blank">{t}Online Store {/t}</a>
-		</p>
-		<p>{t}Mather LifeWays Institute on Aging provides tools and online courses to support caregivers through education, advice, and valuable insights. Preparation and self-care can lighten the load and elevate the experience of caring for a loved one, increasing the quality of life for all parties involved.{/t}</p>
-		<p class="text-center bold">
-			<a href="http://www.matherlifewaysinstituteonaging.com/aspire/" target="_blank">{t}ASPIRE: Customer Rewards Program{/t}</a>
-		</p>
-		<img class="block center" style="-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px;" src="<?php echo $this->getImagesUrl('170469223sidebar1.png'); ?>" alt="EAP Assocation Logo">
-	</div>
-</div>
-<div class="column-wide">
-	<h2 class="flowers">Login</h2>
-	<p>{t}Please complete the form below to login.{/t}</p>
-	<div class="box-white">
-		<p>
-			<?php echo $this->renderPartial('forms/login_form', array('Login' => $model)); ?>
-		</p>
-	</div>
-	<p>
-		{t}Please click{/t} <strong><?php echo CHtml::link('{t}here{/t}', $this->createUrl('register')); ?></strong> {t}if you still need to <strong>register</strong>, or click<strong>{/t}<?php echo CHtml::link('{t}here{/t}', $this->createUrl('forgotPassword')); ?></strong> {t}if you <strong>forgot your password</strong>.{/t}
-	</p>
-=======
   <h1 class="bottom">{t}Login{/t}</h1>
 </div>
 <div id="sidebar">
@@ -49,5 +21,4 @@ border-bottom-left-radius: 5px;" src="<?php echo $this->getImagesUrl('148950191.
     <p> <?php echo $this->renderPartial('forms/login_form', array('Login' => $model)); ?> </p>
   </div>
   <p> {t}Please click{/t} <strong><?php echo CHtml::link('{t}here{/t}', $this->createUrl('register')); ?></strong> {t}if you still need to <strong>register</strong>, or click<strong>{/t}<?php echo CHtml::link('{t}here{/t}', $this->createUrl('forgotPassword')); ?></strong> {t}if you <strong>forgot your password</strong>.{/t} </p>
->>>>>>> .merge_file_AL71x9
 </div>
--- application/views/user/pages/register.php
+++ application/views/user/pages/register.php
@@ -1,54 +1,6 @@
 <?php $this->breadcrumbs = array('{t}Register{/t}'); ?>
 
 <div class="small-masthead" style="background-image: url(<?php echo $this->getImagesUrl('header-register.png'); ?>);">
-<<<<<<< .merge_file_05IV33
-	<h1 class="bottom">{t}Register{/t}</h1>
-</div>
-<div id="sidebar">
-	<div class="box-sidebar one">
-		<h3>Mather LifeWays Institute on Aging</h3>
-		<p class="text-center bold">
-			<a href="http://www.matherlifewaysinstituteonaging.com/products/" target="_blank">{t}Online Store {/t}</a>
-		</p>
-		<p>{t}Mather LifeWays Institute on Aging provides tools and online courses to support caregivers through education, advice, and valuable insights. Preparation and self-care can lighten the load and elevate the experience of caring for a loved one, increasing the quality of life for all parties involved.{/t}</p>
-		<p class="text-center bold">
-			<a href="http://www.matherlifewaysinstituteonaging.com/aspire/" target="_blank">ASPIRE: Customer Rewards Program</a>
-		</p>
-		<img class="block center" style="-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px;" src="<?php echo $this->getImagesUrl('170469223sidebar1.png'); ?>" alt="EAP Assocation Logo">
-	</div>
-	<div class="box-sidebar one">
-		<h3 class="text-center">{t}Did you know?{/t}</h3>
-		<h5 class="text-center">
-			<a href="http://www.aarp.org/" target="_blank">AARP</a>
-		</h5>
-		<p>
-			<a href="http://www.eapassn.org/files/public/EACCroster2012.pdf" target="_blank">{t}The AARP website provides a list of tips for those providing care at a distance.{/t}</a>
-		</p>
-		<p>
-			<a href="http://www.eapassn.org/" target="_blank">
-				<img class="block center" style="-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px;" src="<?php echo $this->getImagesUrl('162496418sidebar1.png'); ?>" alt="EAP Assocation Logo">
-			</a>
-		</p>
-		<p>{t} Caregiving from afar is no easy task. Here are some helpful tips to keep in mind while contemplating caring for your parent from a distance..{/t}</p>
-		<p class="text-center bold">
-			<a href="http://www.aarp.org/relationships/caregiving-resource-center/info-09-2010/pc_tips_for_long_distance_caregiver.html" target="_blank">{t}Tips for the Long-Distance Caregiver{/t}</a>
-		</p>
-		<p>
-			<a href="http://www.eapassn.org/" target="_blank"></a>
-		</p>
-	</div>
-</div>
-<div class="column-wide">
-	<h2 class="flowers">{t} Register {/t}</h2>
-	<p>{t}Complete the fields below to register. You will be sent a verification email to authenticate your email address.{/t}
-	<p class="bold">{t}Employees of IBM, Exxon Mobil, Merck and Mather LifeWays must register with their work email.{/t}</p>
-	<div class="box-white">
-		<?php echo $this->renderPartial('forms/register_form', $models); ?>
-	</div>
-	<p>
-		{t} Please click{/t} <strong><?php echo CHtml::link('{t}here{/t}', $this->createUrl('resendActivation')); ?></strong> {t} to request you activation email again. {/t}
-	</p>
-=======
   <h1 class="bottom">{t}Register{/t}</h1>
 </div>
 <div id="sidebar">
@@ -76,5 +28,4 @@ border-bottom-left-radius: 5px;" src="<?php echo $this->getImagesUrl('148950191.
   <p class="bold">{t}Employees of IBM, Exxon Mobil, Merck and Mather LifeWays must register with their work email.{/t}</p>
   <div class="box-white"> <?php echo $this->renderPartial('forms/register_form', $models); ?> </div>
   <p> {t} Please click{/t} <strong><?php echo CHtml::link('{t}here{/t}', $this->createUrl('resendActivation')); ?></strong> {t} to request you activation email again. {/t} </p>
->>>>>>> .merge_file_vtj6xZ
 </div>
