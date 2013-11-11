279e69044abccc227e20339432f8f7cb03257fbc
JON WOODALL <ourprojects@hotmail.com>
2013-11-11 00:17:34 -0600

    re-added layout/text revisions

--- application/views/user/pages/register.php
+++ application/views/user/pages/register.php
@@ -6,17 +6,28 @@
 <div id="sidebar">
     <div class="box-sidebar one">
         <h3>Mather LifeWays Institute on Aging</h3>
+
         <p class="text-center bold">
-            <a href="http://www.matherlifewaysinstituteonaging.com/family-caregivers/" target="_blank">{t}Family Caregivers{/t}</a>
+            <a href="http://www.matherlifewaysinstituteonaging.com/family-caregivers/" target="_blank">{t}Family
+                Caregivers{/t}</a>
         </p>
-        <p>{t}More than 40 million Americans provide care for relatives or friends with a chronic illness such as dementia, stroke, or Parkinson&rsquo;s disease. This takes an enormous physical and emotional toll on caregivers.{/t}</p>
-        <p>{t}Mather LifeWays Institute on Aging provides tools and online courses to support caregivers through education, advice, and valuable insights.{/t}</p>
+
+        <p>{t}More than 40 million Americans provide care for relatives or friends with a chronic illness such as
+            dementia, stroke, or Parkinson&rsquo;s disease. This takes an enormous physical and emotional toll on
+            caregivers.{/t}</p>
+
+        <p>{t}Mather LifeWays Institute on Aging provides tools and online courses to support caregivers through
+            education, advice, and valuable insights.{/t}</p>
         <img src="<?php echo $this->getImagesUrl('148950191.png'); ?>" alt="Image">
     </div>
     <div class="box-sidebar two">
         <h3>{t}Aging in Action{/t}</h3>
-        <p>{t}Aging in Action is Mather LifeWays Institute on Aging's monthly e-newsletter and blog containing the latest research news in the field of aging.{/t}</p>
-        <a href="http://twitter.com/aginginaction" target="_blank"> <img class="block center" src="<?php echo $this->getImagesUrl('twitter-bird.png'); ?>" alt="Twitter" /></a>
+
+        <p>{t}Aging in Action is Mather LifeWays Institute on Aging's monthly e-newsletter and blog containing the
+            latest research news in the field of aging.{/t}</p>
+        <a href="http://twitter.com/aginginaction" target="_blank"> <img class="block center"
+                                                                         src="<?php echo $this->getImagesUrl('twitter-bird.png'); ?>"
+                                                                         alt="Twitter"/></a>
     </div>
 </div>
 <div class="column-wide">
