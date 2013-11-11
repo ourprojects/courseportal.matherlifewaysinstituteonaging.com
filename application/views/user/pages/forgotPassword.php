a78b0ae3b33394fc58c1517f544f94a8d9a99aa2
JON WOODALL <ourprojects@hotmail.com>
2013-11-11 00:39:15 -0600

    corrected layout

--- application/views/user/pages/forgotPassword.php
+++ application/views/user/pages/forgotPassword.php
@@ -4,20 +4,17 @@
 	<h1 class="bottom">{t}Forgot your password?{/t}</h1>
 </div>
 <div id="sidebar">
-	<div class="box-sidebar one">
-		<h3>Mather LifeWays Institute on Aging</h3>
-		<p class="text-center bold">
-			<a href="http://www.matherlifewaysinstituteonaging.com/products/" target="_blank">{t}Online Store {/t}</a>
-		</p>
-		<p>{t}Mather LifeWays Institute on Aging provides tools and online courses to support caregivers through education, advice, and valuable insights. Preparation and self-care can lighten the load and elevate the experience of caring for a loved one, increasing the quality of life for all parties involved.{/t}</p>
-		<p class="text-center bold">
-			<a href="http://www.matherlifewaysinstituteonaging.com/aspire/" target="_blank">{t}ASPIRE: Customer Rewards Program{/t}</a>
-		</p>
-		<img class="block center" style="-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px;" src="<?php echo $this->getImagesUrl('170469223sidebar1.png'); ?>" alt="EAP Assocation Logo">
-	</div>
+    <div class="box-sidebar one">
+        <h3>Mather LifeWays Institute on Aging</h3>
+        <p class="text-center bold">
+            <a href="http://www.matherlifewaysinstituteonaging.com/family-caregivers/" target="_blank">{t}Family Caregivers{/t}</a>
+        </p>
+        <p>{t}More than 40 million Americans provide care for relatives or friends with a chronic illness such as dementia, stroke, or Parkinson&rsquo;s disease. This takes an enormous physical and emotional toll on caregivers.{/t}</p>
+        <p>{t}Mather LifeWays Institute on Aging provides tools and online courses to support caregivers through education, advice, and valuable insights.{/t}</p>
+        <img src="<?php echo $this->getImagesUrl('148950191.png'); ?>" alt="Image">
+    </div>
 </div>
 <div class="column-wide">
-	<h2 class="flowers">{t}Forgot Your Password{/t}</h2>
 	<p>{t}Complete the form below and instructions will be sent to the email provided describing how to reset your password.{/t}</p>
 	<div class="box-white">
 		<?php echo $this->renderPartial('forms/username_form', $models); ?>
