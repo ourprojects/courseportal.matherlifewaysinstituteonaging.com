2ab870b70e0a59842f01692a3ec73312cff5e01c
louis89 <l.daprato@gmail.com>
2013-11-12 18:31:27 -0600

    Cleaned up old contact code

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -69,38 +69,6 @@ class HomeController extends CoursePortalController
 	 */
 	public function actionContact(array $EReCaptchaForm = array(), array $ContactUs = array(), $ajax = null) 
 	{
-		/*$models = array(
-					'ContactUs' => new ContactUs,
-					'EReCaptchaForm' => Yii::createComponent('ext.recaptcha.EReCaptchaForm', Yii::app()->params['reCaptcha']['privateKey'], $this)
-				);
-		
-		if(Yii::app()->getRequest()->getIsPostRequest())
-		{
-			$models['EReCaptchaForm']->setAttributes($EReCaptchaForm);
-			$models['ContactUs']->setAttributes($ContactUs);
-			// if it is ajax validation request
-			if(isset($ajax) && $ajax === 'contact-form') 
-			{
-				echo CActiveForm::validate($models, null, false);
-				return;
-			}
-			elseif($models['EReCaptchaForm']->validate() && $models['ContactUs']->validate())
-			{
-				$message = Yii::app()->mail->getNewMessageInstance();
-				$message->setBody($models['ContactUs']->body, 'text/plain');
-				$message->setSubject($models['ContactUs']->subject);
-				$message->setTo(Yii::app()->params['supportEmail']);
-				$message->setFrom($models['ContactUs']->email);
-				Yii::app()->mail->send($message);
-	
-				Yii::app()->getUser()->setFlash('success', t('Thank you for contacting us. We will respond to your inquiry as soon as possible.'));
-	
-				$models = array(
-						'ContactUs' => new ContactUs,
-						'EReCaptchaForm' => Yii::createComponent('ext.recaptcha.EReCaptchaForm', Yii::app()->params['reCaptcha']['privateKey'], $this)
-					);
-			}
-		}*/
 		$this->render('pages/contact');
 	}
 
