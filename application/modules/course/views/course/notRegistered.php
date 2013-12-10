eb97556033cbad47592f05b59c7555e7868e0815
jonwoodall <ourprojects@hotmail.com>
2013-12-10 17:13:26 -0600

    corrected sidebar, not registered page

--- application/modules/course/views/course/notRegistered.php
+++ application/modules/course/views/course/notRegistered.php
@@ -4,25 +4,15 @@
     <h1 class="bottom">{t}Online Course Access{/t}</h1>
 </div>
 <div id="sidebar">
-    <div class="box-sidebar two">
-        <h3>Mather LifeWays Institute on Aging</h3>
-
-        <p class="text-center bold">
-            <a href="http://www.matherlifewaysinstituteonaging.com/family-caregivers/" target="_blank">{t}Family
-                Caregivers{/t}</a>
-        </p>
-
-        <p>{t}More than 40 million Americans provide care for relatives or friends with a chronic illness such as
-            dementia, stroke, or Parkinson&rsquo;s disease. This takes an enormous physical and emotional toll on
-            caregivers.{/t}</p>
-
-        <p>{t}Mather LifeWays Institute on Aging provides tools and online courses to support caregivers through
-            education, advice, and valuable insights. Preparation and self-care can lighten the load and elevate the
-            experience of caring for a loved one, increasing the quality of life for all parties involved.{/t}</p>
-        <img
-            style="margin: 0px; padding: 0px; -webkit-border-bottom-right-radius: 5px; -webkit-border-bottom-left-radius: 5px; border-bottom-right-radius: 5px; border-bottom-left-radius: 5px;"
-            src="<?php echo $this->getImagesUrl('148950191.png'); ?>" alt="Image">
-    </div>
+<div class="box-sidebar one">
+<h3>Mather LifeWays Institute on Aging</h3>
+<p class="text-center bold">
+<a href="http://www.matherlifewaysinstituteonaging.com/family-caregivers/" target="_blank">{t}Family Caregivers{/t}</a>
+</p>
+<p>{t}More than 40 million Americans provide care for relatives or friends with a chronic illness such as dementia, stroke, or Parkinson&rsquo;s disease. This takes an enormous physical and emotional toll on caregivers.{/t}</p>
+<p>{t}Mather LifeWays Institute on Aging provides tools and online courses to support caregivers through education, advice, and valuable insights.{/t}</p>
+<img src="<?php echo $this->getImagesUrl('148950191.png'); ?>" alt="Image">
+</div>
 </div>
 <div class="column-wide">
     <p style="color: #000; font-size: 24px;">
