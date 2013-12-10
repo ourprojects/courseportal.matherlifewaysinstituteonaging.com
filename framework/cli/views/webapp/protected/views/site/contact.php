6dddc8a965a18770d39202767dae116d9b38bf5b
jonwoodall <ourprojects@hotmail.com>
2013-12-10 17:22:54 -0600

    added additional form field spacing to accomodate longer name and emails on contact form page

--- framework/cli/views/webapp/protected/views/site/contact.php
+++ framework/cli/views/webapp/protected/views/site/contact.php
@@ -39,25 +39,25 @@ If you have business inquiries or other questions, please fill out the following
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name'); ?>
+		<?php echo $form->textField($model,'name', array('size' =>45)); ?>
 		<?php echo $form->error($model,'name'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email'); ?>
+		<?php echo $form->textField($model,'email', array('size' =>45)); ?>
 		<?php echo $form->error($model,'email'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'subject'); ?>
-		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->textField($model,'subject',array('size' =>45)); ?>
 		<?php echo $form->error($model,'subject'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'body'); ?>
-		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>45)); ?>
 		<?php echo $form->error($model,'body'); ?>
 	</div>
 
