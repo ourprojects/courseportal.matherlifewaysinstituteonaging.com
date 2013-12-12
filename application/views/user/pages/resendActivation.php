6484c5aa4e5757dbb2d94bb27da28615e63d8055
jonwoodall <ourprojects@hotmail.com>
2013-12-12 17:19:20 -0600

    syntax correction

--- application/views/user/pages/resendActivation.php
+++ application/views/user/pages/resendActivation.php
@@ -4,8 +4,7 @@
     <h1 class="bottom">{t} Account Activation Email? {/t}</h1>
 </div>
 <div id="single-column">
-    <p>{t}Please complete the form below to have your activation email sent to you again. You must already be registered
-        to receive this email.{/t}</p>
+    <p>{t}Please enter your username or email address below to have your activation email sent to you again. You must already be registered to receive this email.{/t}</p>
         <?php echo $this->renderPartial('forms/username_form', $models); ?>
     <p>
         {t}Please click <?php echo CHtml::link('{t}here{/t}', $this->createUrl('register')); ?> if you still need to register.{/t}
