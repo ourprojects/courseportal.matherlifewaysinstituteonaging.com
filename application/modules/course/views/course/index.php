4cc89b523edb514e007a256507335a4080cd62b8
jonwoodall <ourprojects@hotmail.com>
2013-12-17 16:30:56 -0600

    reset back to original code for testing

--- application/modules/course/views/course/index.php
+++ application/modules/course/views/course/index.php
@@ -5,9 +5,20 @@
 </div>
 <div id="single-column">
 	<h2 class="flowers">{t}Online Courses - Work/Life Balance{/t}</h2>
-	<p>{t}To help individual caregivers transition into their role, be better prepared to manage their care recipients needs, and learn how to effectively practice self-care, Mather LifeWays Institute on Aging has developed online programs that are designed to educate caregivers while fitting into any schedule.{/t}</p>
+	<p>{t}To help individual caregivers transition into their new role, be better prepared to manage their loved one's needs, and learn how to effectively practice self-care, Mather LifeWays Institute on Aging has developed online programs that are designed to educate caregivers while fitting into any schedule.{/t}</p>
 	<hr />
+	<?php foreach($courses as $course):
 
+	// The following 2 if statements are a BAD HACK!!! Need to find a better way here.
+	if($course->name === 'introtocaregivingonline')
+	{
+		echo CHtml::image($this->getImagesUrl('image-hands.png'), '{t}hands{/t}', array('class' => 'image-right'));
+	}
+	else if($course->name === 'carecoachingonline')
+	{
+		echo CHtml::image($this->getImagesUrl('image-grocery.png'), '{t}groceries{/t}', array('class' => 'image-right'));
+	}
+	?>
 	<h3>
 		<?php echo CHtml::link(t($course->title), $this->createUrl($course->name)); ?>
 	</h3>
@@ -21,6 +32,6 @@
 			echo '<li>' . t($objective->text) . '</li>';
 		?>
 	</ul>
-<?php endforeach ?>
 	<hr />
+	<?php endforeach; ?>
 </div>
