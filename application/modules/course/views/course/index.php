d8044ec128a15fc392f644838c77ac45c6eb760a
jonwoodall <ourprojects@hotmail.com>
2013-12-17 19:04:12 -0600

    more text changes

--- application/modules/course/views/course/index.php
+++ application/modules/course/views/course/index.php
@@ -38,8 +38,8 @@
 	<?php endforeach; ?>
 
 <h2 class="flowers">{t}CARE Coaching Online{/t}</h2>
-<p>{t}To help individual caregivers transition into their new role, be better prepared to manage their loved one's needs, and learn how to effectively practice self-care, Mather LifeWays Institute on Aging has developed online programs that are designed to educate caregivers while fitting into any schedule.{/t}</p>
+<p>{t}CARE Coaching Online provides working caregivers with essential tools, knowledge, and skills to effectively deal with the variety of issues arising from caring for their aging parents, relatives, or loved ones. Developed by Mather LifeWays Institute on Aging, CARE Coaching Online improves working caregivers abilities to communicate, advocate, relate, and encourage their older parents or loved ones in making future plans.{/t}</p>
 
 <h2 class="flowers">{t}Spencer Powell Online Training (SPOT){/t}</h2>
-<p>{t}To help individual caregivers transition into their new role, be better prepared to manage their loved one's needs, and learn how to effectively practice self-care, Mather LifeWays Institute on Aging has developed online programs that are designed to educate caregivers while fitting into any schedule.{/t}</p>
+<p>{t}Spencer Powell Online Training is designed to promote cognitive health and healthy lifestyle changes. Fake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake textFake text{/t}</p>
 </div>
