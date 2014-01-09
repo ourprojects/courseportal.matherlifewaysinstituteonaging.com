7b1187605eaeb5e0140f8939d6ed2097a9974b3a
jonwoodall <ourprojects@hotmail.com>
2014-01-09 16:05:32 -0600

    more spencer #1 video dimensions testing

--- application/modules/course/views/course/courses/spot_introduction.php
+++ application/modules/course/views/course/courses/spot_introduction.php
@@ -182,7 +182,7 @@ href="#lesson-1-slide-12" data-fancybox-group="lesson-1" class="hide lesson-1"><
                         'id' => 'test1',
                         'config' => array(
                                           'image' => $this->createDownloadUrl('videos/spencer_introduction/posterhere.png'),
-                                          'width' => '885px',
+                                          'width' => '900px',
                                           'height' => '480px',
                                           'levels' => array(
                                                             array('file' => $this->createDownloadUrl('videos/spencer_introduction/test1.mp4'))
